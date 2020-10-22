74182e93c1ac832cf369dbd3b17e5984f03b2bfc
3ramayana <3ramayana@gmail.com>
2020-10-22 19:08:05 +0900

    Menyalin isi build

---
+++ precache-manifest.c4c0fbdfc8636305cf6a9e43dae659b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2f90df397bba2c5c386aa8b10882db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664faf8d2f6527d1d9ea",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "06cfe6ac259d9515188f",
+    "url": "/static/js/2.734ac4b5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.734ac4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664faf8d2f6527d1d9ea",
+    "url": "/static/js/main.212ab72a.chunk.js"
+  },
+  {
+    "revision": "e8fb2f26e267c67b1068",
+    "url": "/static/js/runtime-main.46b54b2e.js"
+  }
+]);
